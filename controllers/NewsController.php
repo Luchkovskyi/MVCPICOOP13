bf36d36f6c6281b5d42950fdb217bcf76eb71f02
Luchkovskyi <luca131986@gmail.com>
2016-08-09 10:18:45 +0400

    Oll project

---
+++ controllers/NewsController.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../models/News.php';
+
+class NewsController{
+
+    public function actionAll(){
+        $items = News::getAll();
+        include __DIR__ . '/../views/news/all.php';
+    }
+    public function  actionOne(){
+        echo 'actionOne';
+        die;
+        $id=$_GET['id'];
+        $item = News::getOne();
+        include __DIR__. '/../views/news/one.php';
+    }
+}
---
+++ views/news/one.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stas
+ * Date: 08.08.2016
+ * Time: 18:42
+ */
