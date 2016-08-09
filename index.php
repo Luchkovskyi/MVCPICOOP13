4fa7442d513286e3032db744ac295237476365c3
Luchkovskyi <luca131986@gmail.com>
2016-08-09 10:16:03 +0400

    dddd

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$ctrl= isset($_GET['ctrl']) ? $_GET['ctrl']: 'News';
+$act = isset($_GET['act']) ? $_GET['act']: 'All';
+
+$controllerClassName = $ctrl . 'Controller';
+require_once __DIR__ . '/controllers/' . $controllerClassName . '.php';
+
+$controller = new $controllerClassName;
+$method = 'action'. $act;
+$controller->$method();
